cacca4d6a8e529ce4709759ae0c3f733d991cabb
Alexander Chip <alexchip32@gmail.com>
2020-12-22 18:19:14 -0600

    Maquetacion y formulario de checkout

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
-const Checkout = () => <h1>Checkout</h1>
+const Checkout = () => {
+  return (
+    <section className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$ 10</span>
+          </div>
+          <button type='button'>Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $ 10</h3>
+        <button type='button'>Continuar pedido</button>
+      </div>
+    </section>
+  )
+}
 
 export default Checkout
