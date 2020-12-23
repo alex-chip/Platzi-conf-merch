3d5b535f73f42837660e904f997bf70a2b34cc0b
Alexander Chip <alexchip32@gmail.com>
2020-12-22 18:51:05 -0600

    Maquetacion de la informacion de usuario

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
-const Checkout = () => {
-  return (
-    <section className="Checkout">
-      <div className="Checkout-content">
-        <h3>Lista de pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>Item name</h4>
-            <span>$ 10</span>
-          </div>
-          <button type='button'>Eliminar</button>
+const Checkout = () => (
+  <section className="Checkout">
+    <div className="Checkout-content">
+      <h3>Lista de pedidos:</h3>
+      <div className="Checkout-item">
+        <div className="Checkout-element">
+          <h4>Item name</h4>
+          <span>$ 10</span>
         </div>
+        <button type="button">Eliminar</button>
       </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $ 10</h3>
-        <button type='button'>Continuar pedido</button>
-      </div>
-    </section>
+    </div>
+    <div className="Checkout-sidebar">
+      <h3>Precio Total: $ 10</h3>
+      <button type="button">Continuar pedido</button>
+    </div>
+  </section>
   )
-}
 
 export default Checkout
