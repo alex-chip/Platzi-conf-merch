afc06a4ab64d0d33b9f58d946ca5c657ea42bee4
Alex Chip <alexchip32@gmail.com>
2021-01-16 16:53:40 -0600

    useRef en la pagina checkout

--- src/containers/Information/index.js
+++ src/containers/Information/index.js
@@ -1,42 +1,69 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useContext, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import AppContext from '../../context/AppContext'
 
-const Information = () => (
-  <section className="Information">
-    <div className="Information-content">
-      <div className="Information-head">
-        <h2>Informacion de contacto:</h2>
-      </div>
-      <div className="Information-form">
-        <form action="">
-          <input type="text" placeholder="Nombre completo" name="name" />
-          <input type="email" placeholder="Correo Electronico" name="email" />
-          <input type="text" placeholder="Direccion" name="address" />
-          <input type="text" placeholder="Apto" name="apto" />
-          <input type="text" placeholder="Ciudad" name="city" />
-          <input type="text" placeholder="Pais" name="country" />
-          <input type="text" placeholder="Estado" name="state" />
-          <input type="text" placeholder="Codigo postal" name="cp" />
-          <input type="text" placeholder="Telefono" name="phone" />
-        </form>
-      </div>
-      <div className="Information-buttons">
-        <div className="Information-back">Regresar</div>
-        <div className="Information-next">
-          <Link to='/checkout/payment'>Pagar</Link>
+const Information = () => {
+  const { state, addToBuyer } = useContext(AppContext)
+  const form = useRef(null)
+  const { cart } = state
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current)
+    const buyer = {
+      'name': formData.get('name'),
+      'email': formData.get('email'),
+      'address': formData.get('address'),
+      'apto': formData.get('apto'),
+      'city': formData.get('city'),
+      'country': formData.get('country'),
+      'state': formData.get('state'),
+      'cp': formData.get('cp'),
+      'phone': formData.get('phone')
+    }
+    addToBuyer(buyer)
+  }
+
+  return (
+    <section className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form ref={form}>
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="email" placeholder="Correo Electronico" name="email" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo postal" name="cp" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">
+            <Link to='/checkout'>Regresar</Link>
+          </div>
+          <div className="Information-next">
+            <button type='button' onClick={handleSubmit}>Pagar</button>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="Information-sidebar">
-      <h3>Pedido</h3>
-      <div className="Information-item">
-        <div className="Information-element">
-          <h4>ITEM Name</h4>
-          <span>$ 10</span>
-        </div>
+      <div className="Information-sidebar">
+        <h3>Pedido</h3>
+        {cart.map((item) => (
+          <div className="Information-item" key={item.id}>
+            <div className="Information-element">
+              <h4>{item.name}</h4>
+              <span className='icon-money'>{item.price}</span>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default Information
